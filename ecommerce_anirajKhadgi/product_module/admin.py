f0e8516f04a5ba14bfab7146c33708bde7629c9d
Aniraj1 <anirajshahi@gmail.com>
2022-06-04 00:35:19 +0545

    Lab_4

--- ecommerce_anirajKhadgi/product_module/admin.py
+++ ecommerce_anirajKhadgi/product_module/admin.py
@@ -1,6 +1,47 @@
+from itertools import product
 from django.contrib import admin
+from .models import Brand, Category, Product
 
 # Register your models here.
 
-from .models import Brand
-admin.site.register(Brand)
+#admin.site.register(Brand)
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ["name",]
+    search_fields = ["name",]
+    # list_filter = ["brand",]
+    # readonly_fields = ["quantity",]
+    
+    class Meta:
+        model = Brand
+
+admin.site.register(Brand, BrandAdmin)
+
+# admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name",]
+    search_fields = ["name",]
+    # list_filter = ["category",]
+    # readonly_fields = ["quantity",]
+    
+    class Meta:
+        model = Category
+
+admin.site.register(Category, CategoryAdmin)
+
+
+
+
+# admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["image_tag", "name", "price", "brand", "category",]
+    search_fields = ["name", "price", "brand__name", "category__name",]
+    list_filter = ["brand", "category",]
+    #readonly_fields = ["quantity",]
+
+    class Meta:
+        model = Product
+        
+admin.site.register(Product, ProductAdmin)
--- ecommerce_anirajKhadgi/product_module/models.py
+++ ecommerce_anirajKhadgi/product_module/models.py
@@ -1,5 +1,8 @@
+from email.mime import image
+from statistics import mode
 from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.utils.html import mark_safe
 
 # Create your models here.
 
@@ -9,8 +12,33 @@ class Brand(models.Model):
     
 
 
-# class Category(models.Model):
-#     name = models.CharField(max_length=200)
-#     is_active = models.BooleanField()
-#     class Meta:
-#     verbose_name_plural = "Categories"
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    is_active = models.BooleanField()
+    
+    class Meta:
+        verbose_name_plural = "Categories"   #To rename the name
+
+def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    image_url = models.CharField(max_length=500)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    registered_on = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField()
+    
+    
+    def image_tag(self):
+        return mark_safe(f'<img src="{self.image_url}" width="50" height="50" />')
+        image_tag.short_description = "Product" 
+       
+    
+    # it writes it's own name
+    def __str__(self):
+        return self.name
